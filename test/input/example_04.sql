41d513e44e8cdeb1c42c843ba09fe94d8684cb7e
Bea Maia <beamaia.work@gmail.com>
2022-08-06 19:32:43 -0300

    :sparkles: creates examples
    Co-Authored-By: Sophie Dilhon <51868657+AHalic@users.noreply.github.com>

---
+++ test/input/example_04.sql
@@ -0,0 +1,10 @@
+-- example_04 (PostgreSQL 10)
+INSERT INTO "Schedule" ("time", "#t", "op", "attr") VALUES
+(1, 1,  'R',  'X'),
+(2, 1,  'W',  'X'),
+(3, 2,  'R',  'X'),
+(4, 2,  'W',  'X'),
+(5, 2,  'R',  'Y'),
+(6, 2,  'W',  'Y'),
+(7, 1,  'C',  '-'),
+(8, 2,  'C',  '-');
